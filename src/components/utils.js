b84e72f93e650e55a74c5e56f129ddaadbafa06a
Thomas Horn Sivertsen <thomass@cerpus.com>
2021-02-11 10:12:03 +0100

    Fixed typo + updated dependicies

--- src/components/utils.js
+++ src/components/utils.js
@@ -38,7 +38,7 @@ export function escapeHTML(html) {
 
 export function stripHTML(html) {
     const element = document.createElement('div');
-    element.innerHTML = html
+    element.innerHTML = html;
     return element.innerText;
 }
 
