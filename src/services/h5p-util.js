29d2b18d8fdaec99542540b3607fa0ad1225dd37
Oliver Tacke <o.tacke@posteo.de>
2025-12-19 10:40:08 +0100

    Add comment

--- src/services/h5p-util.js
+++ src/services/h5p-util.js
@@ -1,3 +1,7 @@
+/**
+ * Determines if the H5P content is being interacted with using a mouse.
+ * @returns {boolean} True if using mouse, false otherwise.
+ */
 export const isUsingMouse = () => {
   return document.querySelector('.h5p-content')?.classList.contains('using-mouse');
 };
