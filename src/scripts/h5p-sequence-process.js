a7b0657e06ebc8be9a9ca850a356cd6ec80ec1a7
Oliver Tacke <o.tacke@posteo.de>
2026-01-09 18:22:24 +0100

    Remove TODO

--- src/scripts/h5p-sequence-process.js
+++ src/scripts/h5p-sequence-process.js
@@ -7,7 +7,6 @@ import { getSemanticsDefaults } from '@services/h5p-util.js';
 import './h5p-sequence-process.css';
 
 // TODO: Fix drag state when dropped and no statements remain
-// TODO: Define and use custom CSS variables
 
 export default class SequenceProcess extends H5P.EventDispatcher {
   constructor(params, contentId, extras = {}) {
